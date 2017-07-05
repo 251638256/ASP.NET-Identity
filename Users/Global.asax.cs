bc67fa65f03cfa7a1ac8a7ddacca1d983bfea5e2
WangZhengYuan.UOSO <2659920445@qq.com>
2017-07-05 17:02:28 +0800

    待完成:全局权限过滤器

--- Users/Global.asax.cs
+++ Users/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Users.Models;
 
 namespace Users
 {
@@ -13,6 +14,9 @@ namespace Users
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            
+            // 添加全局自定义过滤去
+            GlobalFilters.Filters.Add(new GlobalAuthorizeAttribute());
         }
     }
 }
---
+++ Users/Models/GlobalAuthorizeAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Users.Models {
+    public class GlobalAuthorizeAttribute : AuthorizeAttribute {
+        protected override bool AuthorizeCore(HttpContextBase httpContext) {
+            if (httpContext.Request.ContentLength > 0) {
+                return false;
+            }
+            return base.AuthorizeCore(httpContext);
+        }
+    }
+}
